aabaae8bbc32e0d8500a19b7300a3d814c2a441e
KianS (Kian William Nørgaard Sørensen) <136251567+KianS25R@users.noreply.github.com>
2026-07-04 13:19:51 +0200

    Update kgame.py

--- kgame.py
+++ kgame.py
@@ -8,4 +8,5 @@ class blackjack():
         self.__phand = []
         self.__dhand = []
         for i in range(2):
-            
+            self.__phand.append(self.__gen())
+            self.__dhand.append(self.__gen())
