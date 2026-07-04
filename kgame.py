0578be71cc74e001b22156a2e99d1d9896d50332
KianS (Kian William Nørgaard Sørensen) <136251567+KianS25R@users.noreply.github.com>
2026-07-04 12:35:01 +0200

    Update kgame.py

--- kgame.py
+++ kgame.py
@@ -4,6 +4,6 @@ class blackjack():
     def __init__(self, digit6):
         """creates blackjack instance"""
         super().__init__()
-        self.__gen() = random(0.01, 0.53, digit6)
+        self.__gen = random(0.01, 0.53, digit6)
         self.__phand = []
         self.__dhand = []
