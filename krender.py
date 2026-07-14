91e23d0d3cbaab0b77c2015239fff993476adfa7
KianS (Kian William Nørgaard Sørensen) <136251567+KianS25R@users.noreply.github.com>
2026-07-14 10:53:25 +0200

    Update krender.py

--- krender.py
+++ krender.py
@@ -13,3 +13,7 @@ def yback(distance, angle):
     a = kmath.rad(angle)
     theta = kmath.cos_rad(a)
     return distance*theta
+    
+class kobject3d():
+    def __init__(self):
+        super().__init__()
